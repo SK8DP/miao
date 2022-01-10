289bf206255d48d3a37254660cedf9a10b202a09
GuanZhichao <754809934@qq.com>
2022-01-10 18:45:16 +0800

    compact

--- lodash/sk8dp-lodash.js
+++ lodash/sk8dp-lodash.js
@@ -12,5 +12,18 @@ var sk8dp = {
       result[resIndex++] = array.slice(index, (index += size));
     }
     return result;
-  }
+  },
+  compact: function (array) {
+    let resIndex = 0;
+    const result = [];
+    if (array == null) {
+      return result;
+    }
+    for (const value of array) {
+      if (value) {
+        result[resIndex++] = value;
+      }
+    }
+    return result;
+  },
 }
