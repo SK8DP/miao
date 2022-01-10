375a0ae74bc6c9f809bdd342aa700e2f002463d8
GuanZhichao <754809934@qq.com>
2022-01-10 19:35:35 +0800

    4

--- lodash/sk8dp-lodash.js
+++ lodash/sk8dp-lodash.js
@@ -1,13 +1,13 @@
 var sk8dp = {
   chunk: function (array, size) {
     size = Math.max(size, 0);
-    const length = array == null ? 0 : array.length;
+    let length = array == null ? 0 : array.length;
     if (!length || size < 1) {
       return [];
     }
     let index = 0;
     let resIndex = 0;
-    const result = new Array(Math.ceil(length / size));
+    let result = new Array(Math.ceil(length / size));
     while (index < length) {
       result[resIndex++] = array.slice(index, (index += size));
     }
@@ -15,15 +15,24 @@ var sk8dp = {
   },
   compact: function (array) {
     let resIndex = 0;
-    const result = [];
+    let result = [];
     if (array == null) {
       return result;
     }
-    for (const value of array) {
+    for (let value of array) {
       if (value) {
         result[resIndex++] = value;
       }
     }
     return result;
   },
+  drop: function (array, n = 1) {
+    let length = array == null ? 0 : array.length;
+    return length > 0 ? array.slice(n) : [];
+  },
+  dropRight: function (array, n = 1) {
+    let length = array == null ? 0 : array.length;
+    return length > 0 ? array.slice(0, n == 0 ? length : -n) : [];
+  }, 
+
 }
