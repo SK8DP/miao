005925754fe9af2b351d6aad4acb38e53ffa80ff
GuanZhichao <754809934@qq.com>
2022-01-10 15:41:39 +0800

    lodash

--- lodash/SK8DP-lodash.js
+++ lodash/SK8DP-lodash.js
@@ -1,3 +1,17 @@
 var sk8dp = {
-
+  chunk: function chunk(array, size = 1) {
+    var res = []
+    var count = 0
+    var temp = []
+    for (var i = 0; i < array.length; i++) {
+      temp.push(array[i])
+      count++
+      if (count == size || i == array.length - 1) {
+        res.push(temp)
+        temp = []
+        count = 0
+      }
+    }
+    return res
+  }
 }
