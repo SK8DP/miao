45a640ff967d59047c0d6da8afb47475c7213927
GuanZhichao <754809934@qq.com>
2022-02-16 19:53:26 +0800

    comment

--- lodash/sk8dp-lodash.js
+++ lodash/sk8dp-lodash.js
@@ -46,7 +46,7 @@ var sk8dp = {
   flattenDeep: function (array) {//原理请见1.12代码
     return array.reduce((result, item) => {
       if (Array.isArray(item)) {//如果元素是数组
-        return result.concat(sk8dp["flattenDeep"](item))//括号里写成sk8dp.flattenDeep(item)也可以，或者写成this.flattenDeep(item)也可以，或者也可以把开头那里的函数声明加个名字即写成flattenDeep:function flattenDeep(array)，然后这里就直接写flattenDeep(item)也可以。除此以外，还有一直更好的写法，请见视频1.17-NO1前半部分
+        return result.concat(sk8dp["flattenDeep"](item))//括号里写成sk8dp.flattenDeep(item)也可以，或者写成this.flattenDeep(item)也可以，或者也可以把开头那里的函数声明加个名字即写成flattenDeep:function flattenDeep(array)，然后这里就直接写flattenDeep(item)也可以。除此以外，还有一些更好的写法，但得需要彻底更改这个sk8dp-lodash.js文件的结构，这里就不方便记录了，请见视频1.17-NO1前半部分。其实以上讲的这一堆写法都是JS对象写法的基础知识的灵活运用而已，如果不理解只能说明JS对象的基础知识学得太不扎实！  另外，刚才讲的这些都是在这个sk8dp-lodash.js文件的函数里通过递归调用自己的写法，现在我再补充讲解一下，在这个sk8dp-lodash.js文件的函数里调用其他以及实现了的函数的写法，比如：我想在这里调用之前写好的compact()函数，那不能直接写compact()，应该写成this.compact()，或者也可以写成sk8dp.compact()，除此以外，还有一些更好的写法，但得需要彻底更改这个sk8dp-lodash.js文件的结构，这里就不方便记录了，请见视频1.17-NO1前半部分。其实以上讲的这一堆写法都是JS对象写法的基础知识的灵活运用而已，如果不理解只能说明JS对象的基础知识学得太不扎实！
       }
       return result.concat(item)//如果元素是非数组
     }, [])
